b1ac5795afbfbebdabecca0bc1898284766fd4c2
Nagaraja LM <naguait.85@gmail.com>
2022-08-13 15:54:01 +0000

    adding knative ser files

--- knative_serv/python/app.py
+++ knative_serv/python/app.py
@@ -4,23 +4,26 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-apiVersion: serving.knative.dev/v1
-kind: Service
-metadata:
-  name: helloworld
-  namespace: default
-spec:
-  template:
-    spec:
-      containers:
-        - image: docker.io/meteatamel/helloworld:v1
-          env:
-            - name: TARGET
-              value: "v1"
+
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    target = os.environ.get('TARGET', 'World')
+    return f'Hello {target}\n'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
